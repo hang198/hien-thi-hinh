be64f656214c91a9eaf25128a3fcc80190b52ab2
hang198 <hang17111998@gmail.com>
2019-07-29 15:45:54 +0700

    hien thi hinh

---
+++ hienthihinh.php
@@ -0,0 +1,26 @@
+<?php
+for ($i=0; $i<7;$i++){
+    echo "<br>";
+    for ($j=1;$j<$i;$j++){
+        echo "*";
+    }
+}
+echo "<br>"
+?>
+<?php
+for ($i=0; $i<3;$i++){
+    echo "<br>";
+    for ($j=0;$j< 7;$j++){
+        echo "*";
+    }
+}
+echo "<br>"
+?>
+<?php
+for ($i=0; $i<7;$i++){
+    echo "<br>";
+    for ($j=0;$j<=$i;$j++){
+        echo "*";
+    }
+}
+?>
